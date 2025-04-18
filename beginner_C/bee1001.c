876d0bb9d6b750596237ce3db1d71b69523501b6
ZimboSebastiao <gloriasebastiao7@gmail.com>
2025-04-18 01:14:19 -0300

    Initial commit

---
+++ beginner_C/bee1001.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    int a;
+    int b;
+    int x;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+	x = (a + b);
+    
+    
+    printf("X = %d\n", x);
+    return 0;
+}
