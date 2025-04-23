3ceb02fef52ec8c7dbc6a7a958d3284639b1fcf2
ZimboSebastiao <gloriasebastiao7@gmail.com>
2025-04-23 20:50:54 -0300

    exercício Bee 1006

---
+++ beginner_C/bee1006.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+	double	a;
+	double	b;
+	double	c;
+	double	media;
+
+	scanf("%lf", &a);
+	scanf("%lf", &b);
+	scanf("%lf", &c);
+
+	a = a * 2;
+	b = b * 3;
+	c = c * 5;
+
+	media = (a + b + c) / 10;
+	printf("MEDIA = %.1lf\n", media);
+    return (0);
+}
