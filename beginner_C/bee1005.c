dba29fcdeb5dd7f0653779c5f501205775345d87
ZimboSebastiao <gloriasebastiao7@gmail.com>
2025-04-23 20:41:52 -0300

    exercício Bee 1005

---
+++ beginner_C/bee1005.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+	double	a;
+	double	b;
+	double	media;
+
+	scanf("%lf", &a);
+	scanf("%lf", &b);
+
+	a = a * 3.5;
+	b = b * 7.5;
+
+	media = (a + b) / 11;
+	printf("MEDIA = %.5lf\n", media);
+    return 0;
+}
