3a03302d83bbe561f8af57138ec48541721541ec
ZimboSebastiao <gloriasebastiao7@gmail.com>
2025-04-26 03:19:38 -0300

    BEE 1011 concluído

---
+++ beginner_C/bee1011.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+	double	volume;
+	double	pi;
+	double	raio;
+
+	pi = 3.14159;
+	scanf("%lf", &raio);
+
+	volume = ((4/3.0) * pi) * (raio * raio * raio);
+	printf("VOLUME = %.3lf\n", sphere);
+    return 0;
+}
